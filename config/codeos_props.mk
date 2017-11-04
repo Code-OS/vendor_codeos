cbeff476f22ccec113656c8ac7b07aa797ff5de9
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-11-04 05:33:20 +0000

    codeosprops: add persist.sys.disable_rescue=true
    Signed-off-by: Rohit Jaiswal <jaiswal.rohit6@gmail.com>

--- config/codeos_props.mk
+++ config/codeos_props.mk
@@ -14,4 +14,5 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.vendor.overlay.theme=com.google.android.theme.pixel \
-    ro.build.selinux=0
+    ro.build.selinux=0 \
+    persist.sys.disable_rescue=true
