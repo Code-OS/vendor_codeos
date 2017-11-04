295b93eb8330e9360a059d91c0b07a165337711e
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-11-04 14:49:49 +0000

    common: activating the Offling Charging Animations
    Signed-off-by: Rohit Jaiswal <jaiswal.rohit6@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -129,13 +129,13 @@ PRODUCT_COPY_FILES +=  \
 #endif
 
 # Custom off-mode charger
-#ifneq ($(WITH_CM_CHARGER),false)
-#PRODUCT_PACKAGES += \
-#    charger_res_images \
-#    cm_charger_res_images \
-#    font_log.png \
-#    libhealthd.cm
-#endif
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
 
 # DU Utils library
 #PRODUCT_BOOT_JARS += \
