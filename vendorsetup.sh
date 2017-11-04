9764e07582cc70101b5425858b1dd61682364010
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-11-04 05:18:08 +0000

    vendorsetup: enable user, eng and userdebug build variants
    Signed-off-by: Rohit Jaiswal <jaiswal.rohit6@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
-. "$(gettop)/vendor/codeos/build/envsetup.sh"
-
-codeos_device_combos
+for device in $(cat vendor/codeos/codeos.devices)
+do
+add_lunch_combo codeos_$device-userdebug
+add_lunch_combo codeos_$device-user
+add_lunch_combo codeos_$device-eng
+done
