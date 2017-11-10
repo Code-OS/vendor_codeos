972faf124f5b56069bbf8884bebf65ee14943a7f
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-11-10 13:25:43 +0530

    prebuilts: correcting  typo

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 217, CodeOS
+# Copyright (c) 2107, CodeOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
